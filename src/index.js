e9937ebf4a5fc231c9f97104cca97f0b03128b79
Saikat Chakraborty <saikatgpae@gmail.com>
2022-09-29 14:37:54 +0530

    Add linters.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
-//component file
-import TodoContainer from "./funtionBased/components/TodoContainer"
+import React from 'react';
+import ReactDOM from 'react-dom';
+// component file
+import TodoContainer from './funtionBased/components/TodoContainer';
 
-
-//stylesheet
-import "./funtionBased/components/App.css"
+// stylesheet
+import './funtionBased/components/App.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <TodoContainer />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
